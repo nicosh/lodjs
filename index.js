c6be7e848d0972844b2642f816b1b6eebaa18781
Nicolò <nico.cozzani@gmail.com>
2019-06-11 17:22:25 +0200

    better json output

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const {call} = require('./lodjs');
+const {call, getUrl} = require('./lodjs');
+// esempio :  Popolazione straniera suddivisa per località nel comune di roma
 const cfg = {
     livello_aggregazione_dati : 2,
     variabili_censuarie : 1,
     location : "Roma"
 }
+// built in functionality, fetch the data and run a callback
 let data = call(cfg,(result)=>{
-    console.log(result)
+    console.log(result.headers) // totale residenti e totale query => { totale: 1090239, totale_residenti: 2617175 }
+    //console.log(result.payload) // elenco risultati query per livello aggregrazione
+})
+
+// custom fetching
+let endpoint = getUrl(cfg)
+fetch(endpoint).then(response => {
+    response.json().then((data) =>{
+        console.log(data)
+        // do whatever you want with the raw data
+    } )
 })
--- lodjs/index.js
+++ lodjs/index.js
@@ -4,17 +4,24 @@ let jsonData = require('./loc.json');
 const {maps} = require('./map.js');
 
 const filledMaps = (cfg) => maps(cfg.location)
-
 const desiredMap = (cfg,filled) => filled[cfg.livello_aggregazione_dati][cfg.variabili_censuarie]
-
+const totalLocPopulation = (cfg)=>{
+    let currentLoc = cfg.location
+    let total = false
+    jsonData.forEach(el=>{ 
+        if(el.FIELD6.toLowerCase() == currentLoc.toLowerCase() || el.FIELD7.toLowerCase() == currentLoc.toLowerCase()){
+            total =  el.FIELD20
+        }
+    })
+    return total
+}
 const getMap = (cfg) => {
     let tmpmap = filledMaps(cfg)
     let theMap = desiredMap(cfg,tmpmap)
     return theMap
 }
-
 const getUrl = (cfg) => {
-    locCode = getLocCode(cfg)
+    let locCode = getLocCode(cfg)
     if(!locCode){
         throw new Error("Località non valida");
     }
@@ -22,7 +29,6 @@ const getUrl = (cfg) => {
     let format = cfg.format ? cfg.format : "json"
     return `http://datiopen.istat.it/sparql/oracle_${locCode}?query=${encodeURIComponent(query)}&format=${format}`
 }
-
 const beautifyResult = (obj)=>{
     let headers = obj.head.vars
     let payload = obj.results.bindings
@@ -35,7 +41,6 @@ const beautifyResult = (obj)=>{
     })
     return data
 }
-
 const getLocCode = (cfg)=>{
     let currentLoc = cfg.location
     code = false
@@ -47,12 +52,26 @@ const getLocCode = (cfg)=>{
     return code
 }
 
+const buildHeadAndPayload = (cfg,payload) =>{
+    let tot = 0 
+    payload.forEach(el=>{ 
+        tot += Number(el.pop)
+    })
+    return {
+        headers : {
+            totale : tot,
+            totale_residenti : Number(totalLocPopulation(cfg).split('.').join(""))
+        },
+        payload
+    }
+}
 const call = (cfg,cb) =>{
     let endpoint = getUrl(cfg)
     fetch(endpoint).then(response => {
         response.json().then((data) =>{
            let cleanresult = beautifyResult(data)
-           cb(cleanresult)
+           let totalPopulation = buildHeadAndPayload(cfg,cleanresult)
+           cb(totalPopulation)
         } )
     })
 } 
